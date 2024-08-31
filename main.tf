f2d823c16eb56a1a76b6d205f1e535e3625449b8
Jimmy Hildingsson <jimmy.hildingsson@redeploy.com>
2024-08-31 11:55:23 +0200

    Moved Required Version to top of the code.

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 terraform {
+  required_version = ">= 0.14.0"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "4.0.1"
-      required_version = "4.0.1"
     }
   }
 }
