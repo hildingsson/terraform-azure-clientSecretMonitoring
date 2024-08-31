383f7047d9b258fed5eb9adcc7b0c4c071d4b6e8
Jimmy Hildingsson <jimmy.hildingsson@redeploy.com>
2024-08-31 12:28:01 +0200

    Changed Unique Identifiers for Resource Groups

--- main.tf
+++ main.tf
@@ -20,19 +20,19 @@ resource "azurerm_resource_group" "resource_group_connectivity" {
   tags     = var.tags_prod_connectivity
 }
 
-resource "azurerm_resource_group" "resource_group_connectivity" {
+resource "azurerm_resource_group" "resource_group_management" {
   name     = "rg-prod-lz-management"
   location = "Sweden Central"
   tags     = var.tags_prod_management
 }
 
-resource "azurerm_resource_group" "resource_group_connectivity" {
+resource "azurerm_resource_group" "resource_group_identity" {
   name     = "rg-prod-lz-identity"
   location = "Sweden Central"
   tags     = var.tags_prod_identity
 }
 
-resource "azurerm_resource_group" "resource_group_connectivity" {
+resource "azurerm_resource_group" "resource_group_app-registration-monitoring" {
   name     = "rg-test-lz-app-registration-monitoring"
   location = "Sweden Central"
   tags     = var.tags_test_app-registration-monitoring
