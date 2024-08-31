21f7023fc6107e0bc2a23b2e1cd08d69aee914f9
Jimmy Hildingsson <jimmy.hildingsson@redeploy.com>
2024-08-31 12:06:32 +0200

    Removed Terraform code for Resource Group as this will be a separate PR.

--- main.tf
+++ main.tf
@@ -13,9 +13,3 @@ provider "azurerm" {
   subscription_id = var.subscription_id
   features {}
 }
-
-resource "azurerm_resource_group" "example" {
-  name     = "my-resource-group"
-  location = "West Europe"
-  tags     = var.tags
-}
